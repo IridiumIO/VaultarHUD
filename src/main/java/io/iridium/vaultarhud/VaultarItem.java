27d37377ab098d18ed5019fa88f47f2a36e097aa
IridiumIO <iridium.io@outlook.com>
2024-04-09 20:40:47 +1000

    Code cleanup

--- src/main/java/io/iridium/vaultarhud/VaultarItem.java
+++ src/main/java/io/iridium/vaultarhud/VaultarItem.java
@@ -1,29 +1,14 @@
 package io.iridium.vaultarhud;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
-
 import java.util.List;
 import java.util.Objects;
 
 class VaultarItem {
-//    private List<String> tags;
     private List<ItemStack> items;
     private int currentTagIndex;
     private int countCompleted;
     private int countTotal;
-
-//    public VaultarItem(List<String> tags, int countCompleted, int countTotal) {
-//        this.tags = tags;
-//        this.currentTagIndex = 0;
-//        this.countCompleted = countCompleted;
-//        this.countTotal = countTotal;
-//        for (String tag : tags) {
-//            items.add(getItemStackFromTag(tag));
-//        }
-//    }
 
     public VaultarItem(List<ItemStack> items, int countCompleted, int countTotal) {
         this.currentTagIndex = 0;
@@ -32,27 +17,15 @@ class VaultarItem {
         this.items = items;
     }
 
-//    public String getCurrentTag() {
-//        return tags.get(currentTagIndex);
-//    }
-
     public ItemStack getCurrentItem() {return items.get(currentTagIndex);}
-
     public void updateIndex() {
         currentTagIndex = (currentTagIndex + 1) % items.size();
     }
-
     public int getCountCompleted() {
         return countCompleted;
     }
-
     public int getCountTotal() {
         return countTotal;
-    }
-
-    private static ItemStack getItemStackFromTag(String tag){
-        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(tag));
-        return new ItemStack(item);
     }
 
     @Override
