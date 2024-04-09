51197510b3ef810030c3af476d0d985aeff00da2
IridiumIO <iridium.io@outlook.com>
2024-04-09 20:39:23 +1000

    Code cleanup

--- src/main/java/io/iridium/vaultarhud/VaultarHud.java
+++ src/main/java/io/iridium/vaultarhud/VaultarHud.java
@@ -4,42 +4,22 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import iskallia.vault.altar.AltarInfusionRecipe;
 import iskallia.vault.altar.RequiredItems;
-import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.common.ForgeConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
-import io.iridium.vaultarhud.VaultarItem;
 
 import iskallia.vault.world.data.PlayerVaultAltarData;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -73,11 +53,7 @@ class VaultarHUDOverlay {
 
     private static List<VaultarItem> vaultarItems = new CopyOnWriteArrayList<>(Arrays.asList());
     private static long lastChangeTime = 0;
-
-    private static ResourceLocation hudTexture = new ResourceLocation(VaultarHud.MOD_ID, "textures/hud.png");
     private static ResourceLocation hudTexture2 = new ResourceLocation(VaultarHud.MOD_ID, "textures/hud_2.png");
-
-
     private static PoseStack poseStack = new PoseStack();
 
     private static ServerPlayer player;
@@ -92,25 +68,25 @@ class VaultarHUDOverlay {
         ServerLevel level = player.getLevel();
         PlayerVaultAltarData altarData = PlayerVaultAltarData.get(level);
 
-        if(altarData == null) {
+        if (altarData == null) {
             return new ArrayList<>();
         }
 
         AltarInfusionRecipe recipe = altarData.getRecipe(player.getUUID());
 
-        if(recipe == null) {
+        if (recipe == null) {
             return new ArrayList<>();
         }
 
         List<RequiredItems> requiredItems = recipe.getRequiredItems();
 
-        if(requiredItems == null) {
+        if (requiredItems.size() == 0) {
             return new ArrayList<>();
         }
 
         List<VaultarItem> vItems = new ArrayList<>();
 
-        for(RequiredItems requiredItem : requiredItems) {
+        for (RequiredItems requiredItem : requiredItems) {
             int amountRequired = requiredItem.getAmountRequired();
             int currentAmount = requiredItem.getCurrentAmount();
             List<ItemStack> reqItems = requiredItem.getItems();
@@ -122,51 +98,6 @@ class VaultarHUDOverlay {
         return vItems;
     }
 
-
-//    @SubscribeEvent
-//    public static synchronized void onRightClickBlock(PlayerInteractEvent.RightClickBlock event){
-//
-//        BlockPos pos = event.getPos();
-//        BlockState blockState = event.getWorld().getBlockState(pos);
-//        Block block = blockState.getBlock();
-//
-//        if (!block.getRegistryName().toString().equals("the_vault:vault_altar")){
-//            return;
-//        }
-//        BlockEntity blockEntity = event.getWorld().getBlockEntity(pos);
-//        if (blockEntity == null){
-//            return;
-//        }
-//
-//        vaultarItems.clear();
-//
-//        CompoundTag nbtData = blockEntity.saveWithFullMetadata();
-//
-//        CompoundTag recipeTag = nbtData.getCompound("Recipe");
-//        ListTag requiredItemsList = recipeTag.getList("requiredItems", 10);
-//
-//        for (Tag rawItemTag : requiredItemsList) {
-//            CompoundTag itemTag = (CompoundTag) rawItemTag;
-//            int amountRequired = itemTag.getInt("amountRequired");
-//            int currentAmount = itemTag.getInt("currentAmount");
-//            ListTag itemsList = itemTag.getList("items", 10);
-//
-//            List<ItemStack> requiredItems = new ArrayList<>();
-//
-//            for (Tag rawSubItemTag : itemsList) {
-//                CompoundTag subItemTag = (CompoundTag) rawSubItemTag;
-//                String id = subItemTag.getString("id");
-//                requiredItems.add(getItemStackFromTag(id));
-//            }
-//
-//            VaultarItem item = new VaultarItem(requiredItems, currentAmount, amountRequired);
-//
-//            vaultarItems.add(item);
-//
-//        }
-//
-//
-//    }
 
     @SubscribeEvent
     public static synchronized void onRenderGameOverlay(RenderGameOverlayEvent event) {
@@ -184,23 +115,15 @@ class VaultarHUDOverlay {
             return;
         }
 
-
+        //Check for altar item updates before replacing the list
         List<VaultarItem> newItems = getVaultarItems(player);
-        if (!newItems.equals(vaultarItems)){
+        if (!newItems.equals(vaultarItems)) {
             vaultarItems = newItems;
         }
 
-//        if (vaultarItems.size() == 0){
-//            vaultarItems = getVaultarItems(player);
-//        } else {
-//            List<VaultarItem> newItems = getVaultarItems(player);
-//            if (!newItems.equals(vaultarItems)){
-//                vaultarItems = newItems;
-//            }
-//        }
 
         // Still check if the list is empty after getting the items
-        if (vaultarItems.size() == 0){
+        if (vaultarItems.size() == 0) {
             return;
         }
 
@@ -217,7 +140,7 @@ class VaultarHUDOverlay {
     }
 
 
-    public static void RenderView1(Minecraft minecraft){
+    public static void RenderView1(Minecraft minecraft) {
 
         // Set the color and enable transparency
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
@@ -231,7 +154,7 @@ class VaultarHUDOverlay {
         int screenWidth = minecraft.getWindow().getGuiScaledWidth();
         int screenHeight = minecraft.getWindow().getGuiScaledHeight();
         int x = screenWidth - 135;
-        int y = screenHeight /2 - (elementSpacing * vaultarItems.size() / 2);
+        int y = screenHeight / 2 - (elementSpacing * vaultarItems.size() / 2);
 
         // Render each item
         for (int i = 0; i < vaultarItems.size(); i++) {
@@ -245,7 +168,7 @@ class VaultarHUDOverlay {
 
     }
 
-    public static void RenderCompositeObjectForView1(PoseStack poseStack, int x, int y, ItemStack itemStack, int countCompleted, int countTotal){
+    public static void RenderCompositeObjectForView1(PoseStack poseStack, int x, int y, ItemStack itemStack, int countCompleted, int countTotal) {
 
         Minecraft minecraft = Minecraft.getInstance();
 
@@ -260,84 +183,22 @@ class VaultarHUDOverlay {
         String text_count = countCompleted + "/" + countTotal;
         int stringWidth = minecraft.font.width(text_count);
         int xOffset = x + 65 - stringWidth / 2;
-        GuiComponent.fill(poseStack, x+6, y+29, (int) Math.floor((x+6) + ((float)countCompleted/countTotal * 118)), y+34, 0xFF00FF00);
+        GuiComponent.fill(poseStack, x + 6, y + 29, (int) Math.floor((x + 6) + ((float) countCompleted / countTotal * 118)), y + 34, 0xFF00FF00);
         minecraft.font.drawShadow(poseStack, new TextComponent(text_count), xOffset, y + 27, 0xFFFFFF);
 
         // Draw the item name, shrinking its size if it is too long
         String text_name = itemStack.getHoverName().getString();
         int nameWidth = minecraft.font.width(text_name);
-        if (nameWidth > 100){
+        if (nameWidth > 100) {
             poseStack.pushPose();
             float scaleF = 100.0f / nameWidth;
             poseStack.scale(scaleF, scaleF, 1.0f);
-            minecraft.font.drawShadow(poseStack, new TextComponent(text_name), (x + 26) /scaleF, (y + 10) /scaleF, 0x54FC54);
+            minecraft.font.drawShadow(poseStack, new TextComponent(text_name), (x + 26) / scaleF, (y + 10) / scaleF, 0x54FC54);
             poseStack.popPose();
-        }else {
-            minecraft.font.drawShadow(poseStack, new TextComponent(text_name), x +26, y + 9, 0x54FC54);
+        } else {
+            minecraft.font.drawShadow(poseStack, new TextComponent(text_name), x + 26, y + 9, 0x54FC54);
         }
 
-
     }
-
-    public static ItemStack getItemStackFromTag(String tag){
-        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(tag));
-        return new ItemStack(item);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @SubscribeEvent
-    public static void onKeyInput(InputEvent.MouseInputEvent event){
-
-        LocalPlayer player = Minecraft.getInstance().player;
-
-        if (player == null){
-            return;
-        }
-
-        //1 = keyDown, 0 = keyUp, 2 = keyRepeat
-        if(!isInventoryOpen() || event.getAction() != 1){
-            return;
-        }
-
-
-        int eventKey = event.getButton();
-
-        KeyMapping inventoryKeybind = Minecraft.getInstance().options.keyInventory;
-
-        int boundKey = inventoryKeybind.getKey().getValue();
-
-        if(eventKey == boundKey){
-            player.closeContainer();
-        }
-
-
-    }
-
-
-
-    public static boolean isInventoryOpen(){
-        Screen screen = Minecraft.getInstance().screen;
-        return screen instanceof AbstractContainerScreen;
-
-    }
-
 
 }
