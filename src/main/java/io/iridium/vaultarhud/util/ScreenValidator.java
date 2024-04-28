0166e112d19a3f93f5b3618393e031a87b73e672
IridiumIO <iridium.io@outlook.com>
2024-04-28 12:28:30 +1000

    Add Inventory views for ColossalChests and SSN

--- src/main/java/io/iridium/vaultarhud/util/ScreenValidator.java
+++ src/main/java/io/iridium/vaultarhud/util/ScreenValidator.java
@@ -5,11 +5,11 @@ import appeng.client.gui.me.items.CraftingTermScreen;
 import com.refinedmods.refinedstorage.screen.grid.GridScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.ContainerScreen;
-import net.minecraft.client.gui.screens.inventory.CraftingScreen;
-import net.minecraft.client.gui.screens.inventory.FurnaceScreen;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.client.gui.screens.inventory.*;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
+import org.cyclops.cyclopscore.client.gui.container.ContainerScreenExtended;
+import com.lothrazar.storagenetwork.block.inventory.ScreenNetworkInventory;
+import com.lothrazar.storagenetwork.block.request.ScreenNetworkTable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,57 +36,56 @@ public class ScreenValidator {
             CraftingTermScreen.class
     );
 
+    public static final List<Class<? extends Screen>> COLOSSALCHEST_SCREENS = Arrays.asList(
+            ContainerScreenExtended.class
+    );
+
+    public static final List<Class<? extends Screen>> SSN_SCREENS = Arrays.asList(
+            ScreenNetworkInventory.class,
+            ScreenNetworkTable.class
+    );
+
 
     public static boolean isValidScreen(Screen screen){
         return isScreenInList(screen, VANILLA_SCREENS) ||
                 isScreenInList(screen, RS_SCREENS) ||
                 isScreenInList(screen, SOPHISTICATED_BACKPACK_SCREENS) ||
-                isScreenInList(screen, AE2_SCREENS);
+                isScreenInList(screen, AE2_SCREENS) ||
+                isScreenInList(screen, COLOSSALCHEST_SCREENS) ||
+                isScreenInList(screen, SSN_SCREENS);
     }
 
     public static boolean isScreenInList(Screen screen, List<Class<? extends Screen>> screenList){
         return screenList.stream().anyMatch(cls -> cls.isInstance(screen));
     }
 
-    public static Point getScreenHUDCoordinates(Screen screen){
+    public static Point getScreenHUDCoordinates(Screen screen, Point offset){
         int screenWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
         int screenHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
-        int x = 0;
-        int y = 0;
-        int offset = 0;
-
-        String screenName = screen.getClass().getName();
+        double x = 0;
+        double y = 0;
 
         if (isScreenInList(screen, VANILLA_SCREENS)){
-            x = ((screenWidth - 176) / 2) - 27;
-            y = ((screenHeight + 166) / 2) - 87;
+            x = ((screenWidth - 176) / 2) - offset.getX();
+            y = ((screenHeight + 166) / 2) - offset.getY();
+        }
 
+        if (isScreenInList(screen, SOPHISTICATED_BACKPACK_SCREENS)){
+            AbstractContainerScreen gs = (AbstractContainerScreen) screen;
+            x = ((screenWidth - 176) / 2) - offset.getX(); //Need to use fixed width here due to different sizes of backpacks
+            y = ((screenHeight + gs.getYSize()) / 2) - offset.getY();
         }
 
         if (isScreenInList(screen, RS_SCREENS)){
-
-            GridScreen gs = (GridScreen) screen;
-
-            x = ((screenWidth - 227) / 2) - 27;
-            y = ((screenHeight + gs.getYSize()) / 2) - 88;
+            AbstractContainerScreen gs = (AbstractContainerScreen) screen;
+            x = ((screenWidth - gs.getXSize()) / 2) - offset.getX();
+            y = ((screenHeight + gs.getYSize()) / 2) - offset.getY() -1;
         }
 
-
-        if (isScreenInList(screen, SOPHISTICATED_BACKPACK_SCREENS)){
-
-            StorageScreenBase gs = (StorageScreenBase) screen;
-
-            x = ((screenWidth - 176) / 2) - 27;
-            y = ((screenHeight + gs.getYSize()) / 2) - 87;
-        }
-
-        if (isScreenInList(screen, AE2_SCREENS)){
-
-            MEStorageScreen gs = (MEStorageScreen) screen;
-            gs = (gs == null) ? (CraftingTermScreen) screen : gs;
-
-            x = ((screenWidth - 195) / 2) - 27;
-            y = ((screenHeight + gs.getYSize()) / 2) - 88;
+        if (isScreenInList(screen, AE2_SCREENS) || isScreenInList(screen, COLOSSALCHEST_SCREENS) || isScreenInList(screen, SSN_SCREENS)){
+            AbstractContainerScreen gs = (AbstractContainerScreen) screen;
+            x = ((screenWidth - gs.getXSize()) / 2) - offset.getX();
+            y = ((screenHeight + gs.getYSize()) / 2) - offset.getY();
         }
 
         return new Point(x, y);
