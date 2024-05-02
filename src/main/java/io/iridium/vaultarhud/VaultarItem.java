5d25d576a94d8d30e00a12026724be0722736c44
IridiumIO <iridium.io@outlook.com>
2024-05-02 14:34:38 +1000

    Refactor VaultarItem class

--- src/main/java/io/iridium/vaultarhud/VaultarItem.java
+++ src/main/java/io/iridium/vaultarhud/VaultarItem.java
@@ -20,40 +20,28 @@ import static io.iridium.vaultarhud.util.SharedFunctions.getItemStackFromTag;
 
 public class VaultarItem {
     private List<ItemStack> items;
-//    private int currentTagIndex;
-    private int countCompleted;
     private int countTotal;
+    private int countCompleted;
 
     public VaultarItem(List<ItemStack> items, int countCompleted, int countTotal) {
-        this.countCompleted = countCompleted;
-        this.countTotal = countTotal;
         this.items = items;
+        this.countTotal = countTotal;
+        this.countCompleted = countCompleted;
     }
 
-
     public VaultarItem(CompoundTag itemTag){
-        int amountRequired = itemTag.getInt("amountRequired");
-        int currentAmount = itemTag.getInt("currentAmount");
-        ListTag itemsList = itemTag.getList("items", 10);
-
-        List<ItemStack> requiredItems = new ArrayList<>();
-        for (Tag rawSubItemTag : itemsList) {
+        this.items = new ArrayList<>();
+        for (Tag rawSubItemTag : itemTag.getList("items", 10)) {
             CompoundTag subItemTag = (CompoundTag) rawSubItemTag;
             String id = subItemTag.getString("id");
-            requiredItems.add(getItemStackFromTag(id));
+            this.items.add(getItemStackFromTag(id));
         }
-
-        this.items = requiredItems;
-        this.countCompleted = currentAmount;
-        this.countTotal = amountRequired;
-
+        this.countTotal = itemTag.getInt("amountRequired");;
+        this.countCompleted = itemTag.getInt("currentAmount");;
     }
 
 
     public ItemStack getCurrentItem(int ticker) {return items.get(ticker % items.size());}
-//    public void updateIndex() {
-//        currentTagIndex = (currentTagIndex + 1) % items.size();
-//    }
     public int getCountCompleted() {
         return countCompleted;
     }
@@ -64,40 +52,13 @@ public class VaultarItem {
 
     public static List<VaultarItem> getVaultarItems(ServerPlayer player) {
 
-        if (player == null) {
-            return new ArrayList<>();
-        }
-        ServerLevel level = player.getLevel();
-
-
-        PlayerVaultAltarData altarData = PlayerVaultAltarData.get(level);
-
-        if (altarData == null) {
-            return new ArrayList<>();
-        }
-
-        AltarInfusionRecipe recipe = altarData.getRecipe(player.getUUID());
-
-        if (recipe == null) {
-            return new ArrayList<>();
-        }
-
-        List<RequiredItems> requiredItems = recipe.getRequiredItems();
-
-        if (requiredItems.size() == 0) {
+        if (player == null || PlayerVaultAltarData.get(player.getLevel()) == null || PlayerVaultAltarData.get(player.getLevel()).getRecipe(player.getUUID()) == null) {
             return new ArrayList<>();
         }
 
         List<VaultarItem> vItems = new ArrayList<>();
-
-        for (RequiredItems requiredItem : requiredItems) {
-            int amountRequired = requiredItem.getAmountRequired();
-            int currentAmount = requiredItem.getCurrentAmount();
-            List<ItemStack> reqItems = requiredItem.getItems();
-
-            VaultarItem item = new VaultarItem(reqItems, currentAmount, amountRequired);
-            vItems.add(item);
-
+        for (RequiredItems requiredItem : PlayerVaultAltarData.get(player.getLevel()).getRecipe(player.getUUID()).getRequiredItems()) {
+            vItems.add(new VaultarItem(requiredItem.getItems(), requiredItem.getCurrentAmount(), requiredItem.getAmountRequired()));
         }
         return vItems;
     }
@@ -106,7 +67,6 @@ public class VaultarItem {
 
     // Add a constructor that accepts a FriendlyByteBuf
     public VaultarItem(FriendlyByteBuf buf) {
-//        this.currentTagIndex = buf.readInt();
         this.countCompleted = buf.readInt();
         this.countTotal = buf.readInt();
         int itemSize = buf.readInt();
@@ -116,9 +76,9 @@ public class VaultarItem {
         }
     }
 
+
     // Add a method to write a VaultarItem to a FriendlyByteBuf
     public void write(FriendlyByteBuf buf) {
-//        buf.writeInt(this.currentTagIndex);
         buf.writeInt(this.countCompleted);
         buf.writeInt(this.countTotal);
         buf.writeInt(this.items.size());
@@ -129,49 +89,49 @@ public class VaultarItem {
 
 
 
-    //BROKEN CODE - I DON'T KNOW HOW IT WENT WRONG BUT NOW I HAVE TO USE THE TOSTRING METHOD TO COMPARE THE ITEMS INSTEAD.
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        VaultarItem vaultarItem = (VaultarItem) obj;
-        if (countCompleted != vaultarItem.countCompleted || countTotal != vaultarItem.countTotal) {
-            return false;
-        }
-
-        // Sort the item lists
-        this.items.sort(Comparator.comparing(item -> item.getItem().getRegistryName().toString()));
-        vaultarItem.items.sort(Comparator.comparing(item -> item.getItem().getRegistryName().toString()));
-
-        // Compare the sorted item lists by their registry names
-        return this.items.stream().map(item -> item.getItem().getRegistryName().toString())
-                .equals(vaultarItem.items.stream().map(item -> item.getItem().getRegistryName().toString()));
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(items, countCompleted, countTotal);
-    }
-
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("VaultarItem{");
-        sb.append(", countCompleted=").append(countCompleted);
-        sb.append(", countTotal=").append(countTotal);
-        sb.append(", items=[");
-        for (int i = 0; i < items.size(); i++) {
-            if (i > 0) {
-                sb.append(", ");
-            }
-            sb.append(items.get(i).getItem().getRegistryName());
-        }
-        sb.append("]}");
-        return sb.toString();
-    }
+//    //BROKEN CODE - I DON'T KNOW HOW IT WENT WRONG BUT NOW I HAVE TO USE THE TOSTRING METHOD TO COMPARE THE ITEMS INSTEAD.
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//        if (obj == null || getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        VaultarItem vaultarItem = (VaultarItem) obj;
+//        if (countCompleted != vaultarItem.countCompleted || countTotal != vaultarItem.countTotal) {
+//            return false;
+//        }
+//
+//        // Sort the item lists
+//        this.items.sort(Comparator.comparing(item -> item.getItem().getRegistryName().toString()));
+//        vaultarItem.items.sort(Comparator.comparing(item -> item.getItem().getRegistryName().toString()));
+//
+//        // Compare the sorted item lists by their registry names
+//        return this.items.stream().map(item -> item.getItem().getRegistryName().toString())
+//                .equals(vaultarItem.items.stream().map(item -> item.getItem().getRegistryName().toString()));
+//    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(items, countCompleted, countTotal);
+//    }
+//
+//
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("VaultarItem{");
+//        sb.append(", countCompleted=").append(countCompleted);
+//        sb.append(", countTotal=").append(countTotal);
+//        sb.append(", items=[");
+//        for (int i = 0; i < items.size(); i++) {
+//            if (i > 0) {
+//                sb.append(", ");
+//            }
+//            sb.append(items.get(i).getItem().getRegistryName());
+//        }
+//        sb.append("]}");
+//        return sb.toString();
+//    }
 
 }
